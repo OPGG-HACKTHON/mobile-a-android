ca2545fc50a2021e090809b796db287ec7b43f8d
Minseop <alstjqaos@gmail.com>
2021-08-08 16:52:17 +0900

    의존성 정리

--- app/src/main/java/com/opgg/chai/ui/main/MainActivity.kt
+++ app/src/main/java/com/opgg/chai/ui/main/MainActivity.kt
@@ -3,7 +3,9 @@ package com.opgg.chai.ui.main
 import com.opgg.chai.R
 import com.opgg.chai.databinding.ActivityMainBinding
 import com.opgg.chai.ui.base.BaseActivity
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity: BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
 
     override fun initialize() {
