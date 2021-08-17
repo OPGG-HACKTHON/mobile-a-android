5d22556524e692337b89afa6a861da09ed373fe7
Minseop <alstjqaos@gmail.com>
2021-08-18 01:25:56 +0900

    [mobile a#3] RankViewModel 추가

---
+++ app/src/main/java/com/opgg/chai/ui/main/rank/RankViewModel.kt
@@ -0,0 +1,7 @@
+package com.opgg.chai.ui.main.rank
+
+import android.app.Application
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.AndroidViewModel
+
+class RankViewModel @ViewModelInject constructor(application: Application): AndroidViewModel(application)
