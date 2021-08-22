6a696f4e84bf454df1bd9703ec4443dae3b7d97b
songmilee <lsm941010@gmail.com>
2021-08-22 17:30:42 +0900

    [mobile a#4] 학교 검색 화면 추가 및 연결

--- app/src/main/java/com/opgg/chai/ui/auth/join/form/JoinFormFragment.kt
+++ app/src/main/java/com/opgg/chai/ui/auth/join/form/JoinFormFragment.kt
@@ -1,13 +1,41 @@
 package com.opgg.chai.ui.auth.join.form
 
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import androidx.activity.result.contract.ActivityResultContracts
 import com.opgg.chai.R
 import com.opgg.chai.databinding.FragmentJoinFormBinding
 import com.opgg.chai.ui.base.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class JoinFormFragment : BaseFragment<FragmentJoinFormBinding, JoinFormViewModel>() {
-    override var viewModel: JoinFormViewModel
-        get() = JoinFormViewModel()
-        set(value) {}
-    override val layoutRes: Int
-        get() = R.layout.fragment_join_form
+    @Inject override lateinit var viewModel: JoinFormViewModel
+    override val layoutRes: Int = R.layout.fragment_join_form
+    lateinit var arrayAdapter: ArrayAdapter<String>
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = super.onCreateView(inflater, container, savedInstanceState)
+        initLayout()
+        binding.vm = viewModel
+        binding.lifecycleOwner = this
+
+        return view
+    }
+
+    fun initLayout() {
+        arrayAdapter = ArrayAdapter<String>(requireContext(), R.layout.item_spinner_school, resources.getStringArray(R.array.belong_list))
+        binding.belongSpinner.adapter = arrayAdapter
+
+        binding.belongEdt.setOnClickListener { navController.navigate(R.id.action_joinFormFragment_to_joinSearchFragment) }
+    }
 }
--- app/src/main/java/com/opgg/chai/ui/auth/join/form/JoinFormViewModel.kt
+++ app/src/main/java/com/opgg/chai/ui/auth/join/form/JoinFormViewModel.kt
@@ -1,6 +1,18 @@
 package com.opgg.chai.ui.auth.join.form
 
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.android.gms.auth.api.signin.GoogleSignIn
 
-class JoinFormViewModel: ViewModel() {
+class JoinFormViewModel constructor(context: Context) : ViewModel() {
+    private val _emailAddress = MutableLiveData<String>()
+    var lolName = MutableLiveData<String>()
+    val emailAddress: LiveData<String> = _emailAddress
+
+    init {
+        val alreadySign = GoogleSignIn.getLastSignedInAccount(context)
+        _emailAddress.value = alreadySign.email
+    }
 }
