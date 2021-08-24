6dbffe005e8317c5dded333324585228fe287a9d
Minseop <alstjqaos@gmail.com>
2021-08-24 19:55:00 +0900

    [mobile a#8] BottomNavigationBar 추가

--- app/src/main/java/com/opgg/chai/ui/main/MainActivity.kt
+++ app/src/main/java/com/opgg/chai/ui/main/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.opgg.chai.ui.main
 
+import android.view.View
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.opgg.chai.R
 import com.opgg.chai.databinding.ActivityMainBinding
 import com.opgg.chai.ui.base.BaseActivity
@@ -9,6 +12,19 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity: BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
 
     override fun initialize() {
-
+        val navController = supportFragmentManager.findFragmentById(R.id.nav_host_fragment)?.findNavController()
+        navController?.let {
+            binding.navBottomView.setupWithNavController(it)
+            it.addOnDestinationChangedListener { _, destination, _ ->
+                binding.navBottomView.visibility = when(destination.id) {
+                    R.id.homeFragment,
+                    R.id.battleFragment,
+                    R.id.rankFragment,
+                    R.id.rankInSchoolFragment,
+                    R.id.settingFragment -> View.VISIBLE
+                    else -> View.GONE
+                }
+            }
+        }
     }
 }
