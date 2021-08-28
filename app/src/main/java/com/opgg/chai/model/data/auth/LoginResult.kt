fbe1ae6bd95cf65d47fecf27434afd9e405fe95b
songmilee <lsm941010@gmail.com>
2021-08-28 23:57:52 +0900

    [mobile a#10] auth 에 필요한 api 추가

---
+++ app/src/main/java/com/opgg/chai/model/data/auth/LoginResult.kt
@@ -0,0 +1,17 @@
+package com.opgg.chai.model.data.auth
+
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class LoginResult(
+    @Json(name = "accessToken")
+    val accessToken: String,
+    @Json(name = "authFrom")
+    val authFrom: String,
+    @Json(name = "email")
+    val email: String,
+    @Json(name = "message")
+    val message: String
+)
