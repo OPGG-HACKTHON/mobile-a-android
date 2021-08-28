f134faf104247773a128f2a9c43910cee46fab0d
songmilee <lsm941010@gmail.com>
2021-08-28 23:56:56 +0900

    [mobile a#10] Term 부분 체크 검사 로직 추가

--- app/src/main/java/com/opgg/chai/ui/auth/join/terms/JoinTermsAdapter.kt
+++ app/src/main/java/com/opgg/chai/ui/auth/join/terms/JoinTermsAdapter.kt
@@ -7,12 +7,11 @@ import androidx.lifecycle.MutableLiveData
 import androidx.recyclerview.widget.RecyclerView
 import com.opgg.chai.R
 import com.opgg.chai.databinding.ItemTermsBinding
-import com.opgg.chai.model.data.TermItem
+import com.opgg.chai.model.data.auth.TermItem
 
 class JoinTermsAdapter : RecyclerView.Adapter<JoinTermsAdapter.JoinTermsVH>() {
     val termList = ArrayList<TermItem>()
     var isSelectAll: MutableLiveData<Boolean> = MutableLiveData(false)
-    private var cntCheck = 0
 
     inner class JoinTermsVH(val binding: ItemTermsBinding) : RecyclerView.ViewHolder(binding.root) {
         fun bind(termItem: TermItem) {
@@ -36,10 +35,13 @@ class JoinTermsAdapter : RecyclerView.Adapter<JoinTermsAdapter.JoinTermsVH>() {
 
         //전체 체크 관리
         holder.binding.termCheck.setOnCheckedChangeListener { buttonView, isChecked ->
+            termList[position].isChecked = isChecked
+            notifyItemChanged(position)
+
             if (!isChecked) {
-                termList[position].isChecked = isChecked
                 isSelectAll.value = false
-                notifyItemChanged(position)
+            } else {
+                IsCheckAll()
             }
         }
     }
@@ -65,4 +67,13 @@ class JoinTermsAdapter : RecyclerView.Adapter<JoinTermsAdapter.JoinTermsVH>() {
         }
     }
 
+    private fun IsCheckAll() {
+        for (i in termList) {
+            if (!i.isChecked) {
+                isSelectAll.value = false
+                return
+            }
+        }
+        isSelectAll.value = true
+    }
 }
