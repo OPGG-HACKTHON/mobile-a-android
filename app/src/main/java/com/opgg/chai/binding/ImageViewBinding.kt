27781318216e061c3879ac71620c4283ea4ea6aa
Minseop <alstjqaos@gmail.com>
2021-08-30 22:38:00 +0900

    [mobile a#8] object 제거

--- app/src/main/java/com/opgg/chai/binding/ImageViewBinding.kt
+++ app/src/main/java/com/opgg/chai/binding/ImageViewBinding.kt
@@ -4,13 +4,10 @@ import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.opgg.chai.util.extension.loadImage
 
-object ImageViewBinding {
 
-    @JvmStatic
-    @BindingAdapter("image")
-    fun bindImage(view: ImageView, url: String?) {
-        url?.let {
-            view.loadImage(it)
-        }
+@BindingAdapter("image")
+fun bindImage(view: ImageView, url: String?) {
+    url?.let {
+        view.loadImage(it)
     }
 }
--- app/src/main/java/com/opgg/chai/binding/RecyclerViewBinding.kt
+++ app/src/main/java/com/opgg/chai/binding/RecyclerViewBinding.kt
@@ -3,12 +3,7 @@ package com.opgg.chai.binding
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 
-object RecyclerViewBinding {
-
-    @JvmStatic
-    @BindingAdapter("adapter")
-    fun bindAdapter(view: RecyclerView, adapter: RecyclerView.Adapter<*>) {
-        view.adapter = adapter
-
-    }
+@BindingAdapter("adapter")
+fun bindAdapter(view: RecyclerView, adapter: RecyclerView.Adapter<*>) {
+    view.adapter = adapter
 }
