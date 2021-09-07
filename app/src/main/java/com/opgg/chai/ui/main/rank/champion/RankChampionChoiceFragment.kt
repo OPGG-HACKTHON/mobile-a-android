21da90bbfd42457d1a64d1493d5d70f9b32c064e
Minseop <alstjqaos@gmail.com>
2021-09-07 14:13:48 +0900

    [mobile a#19] 챔피언랭킹 Step2 UI 개발

--- app/src/main/java/com/opgg/chai/ui/main/rank/champion/RankChampionChoiceFragment.kt
+++ app/src/main/java/com/opgg/chai/ui/main/rank/champion/RankChampionChoiceFragment.kt
@@ -1,14 +1,17 @@
 package com.opgg.chai.ui.main.rank.champion
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.findNavController
 import com.opgg.chai.R
 import com.opgg.chai.databinding.FragmentRankChampionChoiceBinding
+import com.opgg.chai.databinding.ItemCompareCategoryBinding
 import com.opgg.chai.ui.main.rank.adapters.RankChampionAdapter
 
 class RankChampionChoiceFragment: Fragment() {
@@ -31,17 +34,53 @@ class RankChampionChoiceFragment: Fragment() {
             fragment = this@RankChampionChoiceFragment
             vm = this.vm
             adapter = RankChampionAdapter {
-                rankChampionSubmit.isEnabled = it
+                rankChampionStep1Submit.isEnabled = it
             }
         }
         subscribeObserver()
         vm.loadChampions() // todo dummy data 제거 예정
+        vm.loadCompareCategories()
     }
 
     private fun subscribeObserver() {
         vm.champions.observe(viewLifecycleOwner) {
             binding.adapter?.submitList(it)
         }
+
+        vm.compareCategories.observe(viewLifecycleOwner) {
+            it?.let { compareCategories ->
+                binding.rankChampionCategoryChoiceLayout.setData(compareCategories)
+                binding.rankChampionCategoryChoiceLayout.setListener {  isChecked ->
+                    binding.rankChampionStep2DataSubmit.isEnabled = isChecked
+                }
+            }
+        }
+    }
+    
+    fun onClose(view: View) {
+        view.findNavController().popBackStack()
+    }
+
+    fun onChampionSubmit(view: View) {
+        moveStep2()
+    }
+
+    fun onBack(view: View) {
+        moveStep1()
+    }
+
+    fun onDataSubmit(view: View) {
+
+    }
+
+    private fun moveStep1() {
+        binding.rankChampionChoiceProgress.progress = 50
+        binding.rankChampionStepFlipper.showPrevious()
+    }
+
+    private fun moveStep2() {
+        binding.rankChampionChoiceProgress.progress = 100
+        binding.rankChampionStepFlipper.showNext()
     }
 
 }
--- app/src/main/java/com/opgg/chai/ui/main/rank/champion/RankChampionChoiceViewModel.kt
+++ app/src/main/java/com/opgg/chai/ui/main/rank/champion/RankChampionChoiceViewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.opgg.chai.model.data.ChampionItem
+import com.opgg.chai.model.data.CompareCategoryItem
 
 class RankChampionChoiceViewModel @ViewModelInject constructor(
     application: Application): AndroidViewModel(application) {
@@ -14,9 +15,19 @@ class RankChampionChoiceViewModel @ViewModelInject constructor(
     val champions: LiveData<List<ChampionItem>>
         get() = _champions
 
+    private var _compareCategories = MutableLiveData<List<CompareCategoryItem>>()
+    val compareCategories: LiveData<List<CompareCategoryItem>>
+        get() = _compareCategories
+
     fun loadChampions() {
         _champions.value = (0 until 50).map {
             ChampionItem(id = it, name = "name = $it", image = "")
+        }.toList()
+    }
+
+    fun loadCompareCategories() {
+        _compareCategories.value = (0 until 30).map {
+            CompareCategoryItem(id = it, name = "name = ${it * 10000}")
         }.toList()
     }
 
