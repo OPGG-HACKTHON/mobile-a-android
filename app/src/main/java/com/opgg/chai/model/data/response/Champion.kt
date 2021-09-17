6c1f908946a51f042a4f40d6db506374bab767e6
Minseop <alstjqaos@gmail.com>
2021-09-17 12:04:40 +0900

    [mobile a#fix] change champion image url

--- app/src/main/java/com/opgg/chai/model/data/response/Champion.kt
+++ app/src/main/java/com/opgg/chai/model/data/response/Champion.kt
@@ -17,7 +17,7 @@ data class Champion(
         return ChampionItem(
             id = id,
             image = imageUrl,
-            bigImage = bigImageUrl,
+            bigImage = imageUrl,
             name = name)
     }
 
